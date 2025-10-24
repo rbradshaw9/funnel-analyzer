cc4ebd80ceb02f0eab49e4efcfd61942088cbacf
Ryan Bradshaw <ryanbradshaw@Ryans-MacBook-Pro-2.local>
2025-10-24 12:45:40 -0400

    fix: point pricing checkout buttons to thrivecart

--- frontend/lib/externalLinks.ts
+++ frontend/lib/externalLinks.ts
@@ -1,10 +1,10 @@
 export const FUNNEL_ANALYZER_JOIN_URL = process.env.NEXT_PUBLIC_JOIN_URL ?? '/pricing';
 
 export const FUNNEL_ANALYZER_BASIC_CHECKOUT_URL =
-  process.env.NEXT_PUBLIC_BASIC_CHECKOUT_URL ?? 'https://cart.smarttoolclub.com/funnel-analyzer-basic/';
+  process.env.NEXT_PUBLIC_BASIC_CHECKOUT_URL ?? 'https://funnel-analyzer-pro.thrivecart.com/basic/';
 
 export const FUNNEL_ANALYZER_PRO_CHECKOUT_URL =
-  process.env.NEXT_PUBLIC_PRO_CHECKOUT_URL ?? 'https://cart.smarttoolclub.com/funnel-analyzer-pro/';
+  process.env.NEXT_PUBLIC_PRO_CHECKOUT_URL ?? 'https://funnel-analyzer-pro.thrivecart.com/pro/';
 
 export const FUNNEL_ANALYZER_PURCHASE_SUCCESS_URL =
   process.env.NEXT_PUBLIC_PURCHASE_SUCCESS_URL ?? '/success';
